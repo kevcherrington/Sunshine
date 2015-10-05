ef81c1ceb2d1ca6d673441f96085cf0d0815f39c
KCherrington <kevcherrington@gmail.com>
2015-10-05 15:05:51 -0600

    Made the UI Pretty.

--- app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailFragment.java
@@ -191,6 +191,8 @@ public class DetailFragment extends Fragment implements LoaderManager.LoaderCall
             String description = data.getString(COL_WEATHER_DESC);
             descriptionView.setText(description);
 
+            iconView.setContentDescription(description);
+
             // Read high temperature from cursor and update view
             boolean isMetric = Utility.isMetric(getActivity());
 
